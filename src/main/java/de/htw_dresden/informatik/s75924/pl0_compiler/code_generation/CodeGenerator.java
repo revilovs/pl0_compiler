149f206a7613f4d2967e37f3866c2f576361a2fd
revilovs <olivervons@yahoo.com>
2019-01-03 13:44:15 +0100

    Refactor codeBuffer to ByteBuffer for jumping

--- src/main/java/de/htw_dresden/informatik/s75924/pl0_compiler/code_generation/CodeGenerator.java
+++ src/main/java/de/htw_dresden/informatik/s75924/pl0_compiler/code_generation/CodeGenerator.java
@@ -2,13 +2,13 @@ package de.htw_dresden.informatik.s75924.pl0_compiler.code_generation;
 
 import de.htw_dresden.informatik.s75924.pl0_compiler.lexer.SpecialCharacter;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 
 public class CodeGenerator {
-    private ByteArrayOutputStream codeBuffer;
+    private ByteBuffer codeBuffer;
     private RandomAccessFile outputFile;
 
     private char comparisonOperator;
@@ -30,19 +30,22 @@ public class CodeGenerator {
         outputFile.seek(4);
     }
 
-    public void generateProcedureEntry(int procedureIndex, int variableLength) throws IOException {
-        codeBuffer = new ByteArrayOutputStream();
+    public void generateProcedureEntry(int procedureIndex, int variableLength) {
+        //TODO: make size dynamic
+        codeBuffer = ByteBuffer.allocate(4096);
 
-        codeBuffer.write(OperationCode.ENTRY_PROCEDURE.code);
-        codeBuffer.write(shortToBytes((short) 0));
-        codeBuffer.write(shortToBytes((short) procedureIndex));
-        codeBuffer.write(shortToBytes((short) variableLength));
+        codeBuffer.put(OperationCode.ENTRY_PROCEDURE.code);
+        codeBuffer.put(shortToBytes((short) 0));
+        codeBuffer.put(shortToBytes((short) procedureIndex));
+        codeBuffer.put(shortToBytes((short) variableLength));
     }
 
     public void generateProcedureReturn() throws IOException {
-        codeBuffer.write(OperationCode.RETURN_PROCEDURE.code);
+        codeBuffer.put(OperationCode.RETURN_PROCEDURE.code);
 
-        byte[] codeBytes = codeBuffer.toByteArray();
+        byte[] codeBytes = new byte[codeBuffer.position()];
+
+        System.arraycopy(codeBuffer.array(),0, codeBytes, 0, codeBuffer.position());
 
         byte[] codeLengthBytes = shortToBytes((short) codeBytes.length);
 
@@ -51,74 +54,74 @@ public class CodeGenerator {
         outputFile.write(codeBytes);
     }
 
-    public void generatePushVariableValue(int displacement, int procedureIndex, boolean isLocal, boolean isMain) throws IOException {
+    public void generatePushVariableValue(int displacement, int procedureIndex, boolean isLocal, boolean isMain) {
         if (isMain)
-            codeBuffer.write(OperationCode.PUSH_MAIN_VARIABLE_VALUE.code);
+            codeBuffer.put(OperationCode.PUSH_MAIN_VARIABLE_VALUE.code);
         else if (isLocal)
-            codeBuffer.write(OperationCode.PUSH_LOCAL_VARIABLE_VALUE.code);
+            codeBuffer.put(OperationCode.PUSH_LOCAL_VARIABLE_VALUE.code);
         else
-            codeBuffer.write(OperationCode.PUSH_GLOBAL_VARIABLE_VALUE.code);
+            codeBuffer.put(OperationCode.PUSH_GLOBAL_VARIABLE_VALUE.code);
 
-        codeBuffer.write(shortToBytes((short) displacement));
+        codeBuffer.put(shortToBytes((short) displacement));
 
         if (!isLocal && !isMain)
-            codeBuffer.write(shortToBytes((short) procedureIndex));
+            codeBuffer.put(shortToBytes((short) procedureIndex));
     }
 
-    public void generatePushVariableAddress(int displacement, int procedureIndex, boolean isLocal, boolean isMain) throws IOException {
+    public void generatePushVariableAddress(int displacement, int procedureIndex, boolean isLocal, boolean isMain) {
         if (isMain)
-            codeBuffer.write(OperationCode.PUSH_MAIN_VARIABLE_ADDRESS.code);
+            codeBuffer.put(OperationCode.PUSH_MAIN_VARIABLE_ADDRESS.code);
         else if (isLocal)
-            codeBuffer.write(OperationCode.PUSH_LOCAL_VARIABLE_ADDRESS.code);
+            codeBuffer.put(OperationCode.PUSH_LOCAL_VARIABLE_ADDRESS.code);
         else
-            codeBuffer.write(OperationCode.PUSH_GLOBAL_VARIABLE_ADDRESS.code);
+            codeBuffer.put(OperationCode.PUSH_GLOBAL_VARIABLE_ADDRESS.code);
 
-        codeBuffer.write(shortToBytes((short) displacement));
+        codeBuffer.put(shortToBytes((short) displacement));
 
         if (!isLocal && !isMain)
-            codeBuffer.write(shortToBytes((short) procedureIndex));
+            codeBuffer.put(shortToBytes((short) procedureIndex));
     }
 
-    public void generatePushConstant(int constantIndex) throws IOException {
-        codeBuffer.write(OperationCode.PUSH_CONST.code);
+    public void generatePushConstant(int constantIndex) {
+        codeBuffer.put(OperationCode.PUSH_CONST.code);
 
-        codeBuffer.write(shortToBytes((short) constantIndex));
+        codeBuffer.put(shortToBytes((short) constantIndex));
     }
 
     public void generateStoreValue() {
-        codeBuffer.write(OperationCode.STORE_VALUE.code);
+        codeBuffer.put(OperationCode.STORE_VALUE.code);
     }
 
     public void generatePutValue() {
-        codeBuffer.write(OperationCode.PUT_VALUE.code);
+        codeBuffer.put(OperationCode.PUT_VALUE.code);
     }
 
     public void generateGetValue() {
-        codeBuffer.write(OperationCode.GET_VALUE.code);
+        codeBuffer.put(OperationCode.GET_VALUE.code);
     }
 
     public void generateNegativeSign() {
-        codeBuffer.write(OperationCode.NEGATIVE_SIGN.code);
+        codeBuffer.put(OperationCode.NEGATIVE_SIGN.code);
     }
 
     public void generateAddOperator(){
-        codeBuffer.write(OperationCode.OPERATOR_ADD.code);
+        codeBuffer.put(OperationCode.OPERATOR_ADD.code);
     }
 
     public void generateSubtractOperator(){
-        codeBuffer.write(OperationCode.OPERATOR_SUBTRACT.code);
+        codeBuffer.put(OperationCode.OPERATOR_SUBTRACT.code);
     }
 
     public void generateMultiplyOperator(){
-        codeBuffer.write(OperationCode.OPERATOR_MULTIPLY.code);
+        codeBuffer.put(OperationCode.OPERATOR_MULTIPLY.code);
     }
 
     public void generateDivideOperator() {
-        codeBuffer.write(OperationCode.OPERATOR_DIVIDE.code);
+        codeBuffer.put(OperationCode.OPERATOR_DIVIDE.code);
     }
 
     public void generateOdd() {
-        codeBuffer.write(OperationCode.ODD.code);
+        codeBuffer.put(OperationCode.ODD.code);
     }
 
     public void setComparisonOperator(char comparisonOperator) {
@@ -126,7 +129,7 @@ public class CodeGenerator {
     }
 
     public void generateComparisonOperator() {
-        codeBuffer.write(comparisonOperatorMap.get(comparisonOperator).code);
+        codeBuffer.put(comparisonOperatorMap.get(comparisonOperator).code);
     }
 
     public void writeConstantBlock(long[] constantBlock) throws IOException {
